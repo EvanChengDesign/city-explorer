02a56ce3208e05de0fcfbc19755bfb196c16c5c8
Evan Cheng <evanchengdesign@gmail.com>
2024-04-03 23:46:51 -0400

    front-end connection of /movies

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import SearchForm from './SearchForm';
 import LocationInfo from './LocationInfo';
 import ErrorMessage from './ErrorMessage';
 import Weather from './Weather';
+import Movies from './Movies.jsx'; 
 
 
 
@@ -10,11 +11,31 @@ function App() {
   const [city, setCity] = useState('');
   const [location, setLocation] = useState({});
   const [weatherData, setWeatherData] = useState ([]);
+  const [movieData, setMovieData] = useState ([]);
   const [error, setError] = useState('');
 
   const accessToken = import.meta.env.VITE_LOCATION_API_KEY;
   
-
+  async function getMovie(lat, lon) {
+    let movieUrl = `http://localhost:3000/movies?location=${city}`;
+    console.log(movieUrl);
+  
+    try {
+      let response = await fetch(movieUrl);
+      let jsonData = await response.json();
+      if (response.ok) {
+        setMovieData(jsonData); 
+        console.log(jsonData);
+        setError('');
+      } else {
+        setError('Failed to fetch movie data');
+      }
+    } catch (error) {
+      console.error("Error fetching movie data:", error);
+      setError("Error fetching movie data");
+    }
+  }
+  
 
   async function getWeather(lat, lon) {
     let weatherUrl = `http://localhost:3000/weather?city=${city}`;
@@ -39,7 +60,7 @@ function App() {
       setLocation({});
       return;
     }
-
+  
     let url = `https://us1.locationiq.com/v1/search?key=${accessToken}&q=${city}&format=json`;
     try {
       let response = await fetch(url);
@@ -47,21 +68,23 @@ function App() {
       console.log(jsonData);
       console.log(jsonData[0]);
       let locationData = jsonData[0];
-
+  
       if (locationData.error) {
         setError("That's not a real city! Check the spelling!");
         setLocation({});
         setWeatherData([]);
+        setMovieData([]); // Make sure to clear movie data on error as well
       } else {
         setLocation(locationData);
         setError('');
+        getWeather(locationData.lat, locationData.lon);
+        getMovie(locationData.lat, locationData.lon); // Now calling getMovie as well
       }
-      getWeather(locationData.lat, locationData.lon);
-
     } catch(error) {
       console.error("Error getting location information", error);
       setError("Error getting location information");
       setWeatherData([]);
+      setMovieData([]); // Make sure to clear movie data on error as well
     }
   }
 
@@ -73,6 +96,8 @@ function App() {
           {error && <ErrorMessage message={error} />}
           {location.display_name && <LocationInfo location={location} accessToken={accessToken} />}
           {weatherData.length > 0 && <Weather forecasts={weatherData} />} 
+          {movieData.length > 0 && <Movies movies={movieData} />}
+
         </div>
       </div>
     </div>
